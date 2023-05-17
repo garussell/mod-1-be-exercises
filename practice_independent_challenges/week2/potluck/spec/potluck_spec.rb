68ee873cd02734fa01c3fa9da81d868ce8099cfd
Allen Russell <garyrusselljr@gmail.com>
2023-05-17 15:06:31 -0600

    test: add test for add_dish method

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe PotLuck do
       expect(potluck).to be_an_instance_of(PotLuck)
     end
   end
+
+  describe "#add_dish" do
+    it 'can add a dish' do
+      potluck = PotLuck.new("7-13-18")
+      couscous_salad = Dish.new("Cocktail Meatballs", :entre)
+      cocktail_meatballs = Dish.new("Couscous Salad", :appetizer)
+
+      potluck.add_dish(couscous_salad)
+      potluck.add_dish(cocktail_meatballs)
+
+      expect(potluck.dishes).to eq(@dishes)
+    end
+  end
 end
